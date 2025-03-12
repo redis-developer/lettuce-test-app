b3b83dbd4cb0f0762002e1b569051af8e771c058
ggivo <ivo.gaydazhiev@redis.com>
2025-03-12 11:42:42 +0200

    Dump configuration formating
    Dump configuration to log file

--- src/main/java/io/lettuce/test/config/ConfigDumper.java
+++ src/main/java/io/lettuce/test/config/ConfigDumper.java
@@ -20,27 +20,26 @@ public class ConfigDumper {
     private static final Logger logger = LoggerFactory.getLogger(ConfigDumper.class);
 
     @EventListener
-    public void handleContextRefresh(ApplicationReadyEvent event) {
+    public void dumpConfiguration(ApplicationReadyEvent event) {
         final Environment env = event.getApplicationContext().getEnvironment();
-        logger.info("====== Environment and configuration ======");
-        logger.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
         final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
-        StreamSupport.stream(sources.spliterator(), false)
-                .filter(ps -> ps instanceof EnumerablePropertySource<?>)
-                .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
-                .flatMap(Arrays::stream)
-                .distinct()
-                .sorted()
-                .forEach(prop -> {
-                    String value = env.getProperty(prop);
-                    if (prop.contains("credentials") || prop.contains("password") || prop.contains("token")) {
-                        value = "<hidden>";
-                    }
 
-                    logger.info("{}: {}", prop, value);
+        StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource<?>)
+                .forEach(ps -> {
+                    // Print the property source name
+                    logger.info("\n-- Property Source: {}", ps.getName());
 
+                    // Log each property from this property source
+                    String[] propertyNames = ((EnumerablePropertySource<?>) ps).getPropertyNames();
+                    Arrays.stream(propertyNames).sorted().forEach(prop -> {
+                        String value = env.getProperty(prop);
+                        // Hide sensitive properties
+                        if (prop.contains("credentials") || prop.contains("password") || prop.contains("token")) {
+                            value = "<hidden>";
+                        }
+                        logger.info("{}: {}", prop, value);
+                    });
                 });
-
-        logger.info("===========================================");
     }
+
 }
