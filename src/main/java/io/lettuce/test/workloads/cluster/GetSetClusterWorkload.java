0108604c6104b7ee3f0699fb990d34fc4b6c52d3
ggivo <ivo.gaydazhiev@redis.com>
2025-03-21 09:20:09 +0200

    Cluster get-set workload configurable option to wait for all command to complete before submitting next batch

--- src/main/java/io/lettuce/test/workloads/cluster/GetSetClusterWorkload.java
+++ src/main/java/io/lettuce/test/workloads/cluster/GetSetClusterWorkload.java
@@ -1,12 +1,17 @@
 package io.lettuce.test.workloads.cluster;
 
+import io.lettuce.core.LettuceFutures;
+import io.lettuce.core.RedisFuture;
 import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
 import io.lettuce.core.cluster.api.async.RedisAdvancedClusterAsyncCommands;
 import io.lettuce.test.CommonWorkloadOptions;
 import io.lettuce.test.util.PayloadUtils;
 import io.lettuce.test.workloads.BaseWorkload;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class GetSetClusterWorkload extends BaseWorkload {
 
@@ -19,6 +24,8 @@ public class GetSetClusterWorkload extends BaseWorkload {
 
     @Override
     public void run() {
+        List<RedisFuture<String>> futures = new ArrayList<>();
+
         RedisAdvancedClusterAsyncCommands<String, String> cmd = withMetrics(conn.async());
         Random random = new Random();
 
@@ -27,12 +34,16 @@ public class GetSetClusterWorkload extends BaseWorkload {
         for (int i = 0; i < options().iterationCount(); i++) {
             String key = keyGenerator().nextKey();
             if (random.nextDouble() < options().getSetRatio()) {
-                cmd.set(key, payload);
+                futures.add(cmd.set(key, payload));
             } else {
-                cmd.get(key);
+                futures.add(cmd.get(key));
             }
 
             delay(options().delayAfterIteration());
+        }
+
+        if (options().getBoolean("awaitAllResponses", true)) {
+            LettuceFutures.awaitAll(1, TimeUnit.MINUTES, futures.toArray(new RedisFuture[futures.size()]));
         }
     }
 
