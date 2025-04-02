2b8271fd8111c930ddebd7e4d9951184c5ab5395
ggivo <ivo.gaydazhiev@redis.com>
2025-04-02 08:01:01 +0300

    Filter metrics published in logs

--- src/main/java/io/lettuce/test/metrics/MeterRegistryConfiguration.java
+++ src/main/java/io/lettuce/test/metrics/MeterRegistryConfiguration.java
@@ -1,6 +1,7 @@
 package io.lettuce.test.metrics;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,6 +24,7 @@ public class MeterRegistryConfiguration {
         };
     }
 
+
     @Bean
     @ConditionalOnProperty(prefix = "logging.metrics", name = "enabled", havingValue = "true", matchIfMissing = true)
     public ExtendedLoggingMeterRegistry loggingMeterRegistry(Environment env) {
@@ -40,14 +42,29 @@ public class MeterRegistryConfiguration {
 
         };
 
-        return ExtendedLoggingMeterRegistry.builder(loggingConfig).build();
+        ExtendedLoggingMeterRegistry registry = ExtendedLoggingMeterRegistry.builder(loggingConfig).build();
+        meterFilters(registry);
+
+        return registry;
     }
 
     @Bean
     @ConditionalOnProperty(prefix = "simple.metrics", name = "enabled", havingValue = "true", matchIfMissing = true)
     public SimpleMeterRegistry simpleMeterRegistry(Environment env) {
 
-        return new SimpleMeterRegistry();
+        SimpleMeterRegistry registry = new SimpleMeterRegistry();
+        meterFilters(registry);
+        return registry;
+    }
+
+    //* Deny all meters by default and accept only specific meters for metrics stored in logs
+    private void meterFilters(MeterRegistry registry) {
+        registry.config()
+                .meterFilter(MeterFilter.acceptNameStartsWith("redis.command"))
+                .meterFilter(MeterFilter.acceptNameStartsWith("lettuce.connect"))
+                .meterFilter(MeterFilter.acceptNameStartsWith("lettuce.reconnect"))
+                .meterFilter(MeterFilter.acceptNameStartsWith("lettuce.reconnection"))
+                .meterFilter(MeterFilter.deny());
     }
 
 }
